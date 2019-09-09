c1d92ff422807449fbfd1f670a915150e43dd58b
llgg404 <llgg404@hotmail.com>
2019-09-09 23:16:53 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const webpack = require('webpack')
 module.exports = {
+	distDir: '_next',
 	webpack: (config) => {
 		config.plugins.push(
 			new webpack.EnvironmentPlugin(process.env)
